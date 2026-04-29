72a2e63bd628c971738ee688b185ea13ad5c3126
Saanvi P Rao <raosaanvi370@gmail.com>
2026-04-29 06:08:54 +0000

    Try fixing this error: `Runtime Error: useFirebase must be used within a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-
 import type { Metadata } from 'next';
 import './globals.css';
-import { FirebaseClientProvider } from '@/firebase/client-provider';
+import { FirebaseClientProvider } from '@/firebase';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
